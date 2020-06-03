9818a799f192eb3cae5fd051ed5be300ab45d3fc
Josh Ayres <joshvayres10@gmail.com>
2020-06-03 16:03:36 -0400

    next button

--- index.js
+++ index.js
@@ -1,77 +1,92 @@
-const express = require('express');
-const cheerio = require('cheerio');
-const fetch = require('node-fetch');
-const getUrls = require('get-urls');
-const recipeScraper = require('recipe-scraper');
-const cors = require('cors');
+const express = require("express");
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const getUrls = require("get-urls");
+const recipeScraper = require("recipe-scraper");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
 var avalWeb = [
-    '101cookbooks.com/',
-    'allrecipes.com/rec',
-    'ambitiouskitchen.com/',
-    'averiecooks.com/',
-    'bbc.co.uk/',
-    'bbcgoodfood.com/',
-    'bonappetit.com/rec',
-    'budgetbytes.com/',
-    'closetcooking.com/',
-    'cookieandkate.com/',
-    'damndelicious.net/',
-    'eatingwell.com/',
-    'epicurious.com/',
-    'finecooking.com/',
-    'food.com/',
-    'foodandwine.com/res',
-//    'foodnetwork.com/',
-    'gimmesomeoven.com/',
-    'kitchenstories.com/',
-    'myrecipes.com/',
-    'nomnompaleo.com/',
-    'seriouseats.com/',
-//    'simplyrecipes.com/',
-    'smittenkitchen.com/',
-    'thepioneerwoman.com/',
-    'therealfoodrds.com/',
-    'thespruceeats.com/',
-    'whatsgabycooking.com/',
-    'woolworths.com.au/',
-    'yummly.com/',
+  "101cookbooks.com/",
+  "allrecipes.com/rec",
+  "ambitiouskitchen.com/",
+  "averiecooks.com/",
+  "bbc.co.uk/",
+  "bbcgoodfood.com/",
+  "bonappetit.com/rec",
+  "budgetbytes.com/",
+  "closetcooking.com/",
+  "cookieandkate.com/",
+  //"damndelicious.net/",
+  "eatingwell.com/",
+  "epicurious.com/",
+  "finecooking.com/",
+  //"food.com/",
+  "foodandwine.com/res",
+  //'foodnetwork.com/',
+  "gimmesomeoven.com/",
+  "kitchenstories.com/",
+  "myrecipes.com/",
+  "nomnompaleo.com/",
+  "seriouseats.com/",
+  //    'simplyrecipes.com/',
+  "smittenkitchen.com/",
+  "thepioneerwoman.com/coo",
+  "therealfoodrds.com/",
+  //"thespruceeats.com/",
+  "whatsgabycooking.com/",
+  "woolworths.com.au/",
+  "yummly.com/",
 ];
 
 const url = "https://www.google.com/search?q=";
 
-app.get('/:food', (req, res) => {
-    var searchurl = url + req.params.food + "recipe";
-    (async () => {
-        const response = await fetch(searchurl);
-        const body = await response.text();
-        var urls = getUrls(body);
-        var urlsStrings = Array.from(urls);
-        urlsStrings.every((element, index) => {
-            var length = avalWeb.length;
-            var i = 0;
-            while (i <= length) {
-                if (element.includes(avalWeb[i])) {
-                    var recUrl = element.substring(0, element.indexOf('&'));
-                    recipeScraper(recUrl).then(recipe => {
-                        res.send(recipe);
-                    }).catch(err => {
-                        console.log(err);
-                        res.send(err);
-                    });
-                    return false;
-                }
-                i++;
-            }
-            return true;
-        });
-    })();
-})
+function getRecipeFromUrl(url) {
+  var recUrl = "";
+  for (var i = 0; i < avalWeb.length; i++) {
+    if (url.includes(avalWeb[i])) {
+      recUrl = url.substring(0, url.indexOf("&"));
+      break;
+    }
+  }
+  return recUrl;
+}
 
-app.listen(8000, () => {
+app.get("/:food/:num", (req, res) => {
+  var searchurl = url + req.params.food + " recipe" + "&start=" + req.params.num;
+  (async () => {
+    const response = await fetch(searchurl);
+    const body = await response.text();
+    var urls = getUrls(body);
+    var urlsStrings = Array.from(urls);
 
+    let ur = '';
+    let num = 0;
+    for (var i = 0; i < urlsStrings.length; i++) {
+      if (getRecipeFromUrl(urlsStrings[i])) {
+        ur = getRecipeFromUrl(urlsStrings[i]);
+        // 18 is how many google urls there are
+        num = i - 12;
+        break;
+      }
+    }
+    console.log(ur);
+    if(ur == '') {
+      console.log("No link found");
+      return;
+    }
+    recipeScraper(ur).then(recipe => {
+      recipe.url = ur;
+      recipe.num = num;
+      res.send(recipe)
+    }).catch(err => {
+      console.log(ur);
+      console.log(err.message);
+    })
+  })();
 });
+
+app.listen(8000, () => {});
