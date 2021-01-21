5231c88939036313154f2224fcd9c8945cac7d1a
Josh Ayres <joshvayres10@gmail.com>
2021-01-21 01:57:16 -0500

    maybe done?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 const getUrls = require("get-urls");
 const recipeScraper = require("recipe-scraper");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -86,6 +87,11 @@ app.get("/api/:food/:num", (req, res) => {
       console.log(err.message);
     })
   })();
+});
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(8000, () => {});
